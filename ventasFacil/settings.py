fe2306f049d0f9ae312303c3913b86883a2d3a33
jeanlol0123 <jeanpaulol0123@gmail.com>
2023-05-23 03:11:57 -0500

    ready for render.com

--- ventasFacil/settings.py
+++ ventasFacil/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = '/opt/render/project/src/staticfiles/'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
