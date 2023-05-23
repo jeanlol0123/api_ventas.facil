926c90ba407c87b9260d82b3a7052a376bb71269
jeanlol0123 <jeanpaulol0123@gmail.com>
2023-05-23 01:57:02 -0500

    ready for render.com

--- Aplicaciones/Principal/urls.py
+++ Aplicaciones/Principal/urls.py
@@ -11,25 +11,6 @@ router.register(r'_proveedores',views.ProveedoresViewSet)
 
 app_name='Principal'
 urlpatterns = [
-    path('', views.base , name='base'),
-    path('inicio/', views.inicio, name='inicio'),
-    path('personal/', views.personal, name='personal'),
-    path('producto/', views.productos, name='productos'),
-    path('proveedores/', views.proveedores, name='proveedores'),
-    path('pedidos/', views.pedidos, name='pedidos'),
-    path('facturas/', views.facturas, name='facturas'),
-    path('verUsuarios/',views.verUsuarios, name='verUsuarios'),
-    path('login/', views.login, name='login'),
-    path('verProductos/',views.verProductos, name='verProductos'),
-    path('verProveedores/',views.verProveedores, name='verProveedores'),
-    #Paths para personal
-    path('registrarUsuario/', views.registrarUsuario),
-    path('eliminarUsuario/<idRegistro>', views.eliminarUsuario),
-    #Paths para producto
-    path('registrarProducto/', views.registrarProducto),
-    #Paths para Proveedores
-    path('registrarProveedor/',views.registrarProveedor),
-    #api by jeanlol
     path('api', include(router.urls)),
 ] 
 
